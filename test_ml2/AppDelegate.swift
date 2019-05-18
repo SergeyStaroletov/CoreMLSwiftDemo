4a8dcaeec3004b41b34b9a4693f8aa45493e402d
Sergey <serg_soft@mail.ru>
2019-05-18 19:46:33 +0700

    Initial Commit

---
+++ test_ml2/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  test_ml2
+//
+//  Created by Sergey Staroletov on 18/05/2019.
+//  Copyright © 2019 Sergey Staroletov. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ test_ml2/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  test_ml2
+//
+//  Created by Sergey Staroletov on 18/05/2019.
+//  Copyright © 2019 Sergey Staroletov. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
